2077cdee1d036d660d3b6732c5be200bc95f85d8
Gabriel <creeplays@gmail.com>
2020-04-06 00:20:56 -0300

    Base de aplicação com angular e electron configuradas.

---
+++ main.js
@@ -0,0 +1,58 @@
+const {app, BrowserWindow} = require('electron');
+const url = require("url");
+const path = require('path');
+
+function createWindow () 
+{
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 650,
+    height: 700,
+    webPreferences: 
+    {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(
+    url.format(
+    {
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () 
+{
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () 
+{
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0)
+  {
+    createWindow();
+  } 
+})
+
+app.allowRendererProcessReuse = true;
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
